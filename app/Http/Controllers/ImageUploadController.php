262052ac11975a8c939f68a84f14ba8612b03377
mohfiqiherinsyah <fiqiherinsyah@bangunindo.com>
2025-02-19 20:08:28 +0700

    update landing gmi

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Storage;
 
 class ImageUploadController extends Controller
 {
@@ -17,13 +19,13 @@ class ImageUploadController extends Controller
             'images.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10240'
         ]);
 
-        if ($request->hasfile('images')) {
+        if ($request->hasFile('images')) {
             foreach ($request->file('images') as $image) {
                 $filename = time() . '_' . $image->getClientOriginalName();
-                $destinationPath = '/home/amaz9546/public_html/garudamestakungindonesia.my.id/public/upload_image';
-                $image->move($destinationPath, $filename);
+                $path = $image->storeAs('public/images', $filename);
+
                 Image::create([
-                    'filepath' => 'public/upload_image/' . $filename,
+                    'filepath' => 'images/' . $filename,
                 ]);
             }
         }
