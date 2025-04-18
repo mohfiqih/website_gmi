8160cf34c1bbd8eeda0e5a67b055a9b0f05406cc
mohfiqiherinsyah <fiqiherinsyah@bangunindo.com>
2025-04-18 21:42:35 +0700

    update v1

--- resources/views/landing/manual-book.blade.php
+++ resources/views/landing/manual-book.blade.php
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('template_manual_book/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_manual_book/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_manual_book/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_manual_book/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('template_manual_book/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/vendor/bootstrap/css/bootstrap.min.css?v=1.0') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/vendor/bootstrap-icons/bootstrap-icons.css?v=1.0') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/vendor/aos/aos.css?v=1.0') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/vendor/glightbox/css/glightbox.min.css?v=1.0') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/vendor/swiper/swiper-bundle.min.css?v=1.0') }}" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="{{ asset('template_manual_book/assets/css/main.css') }}" rel="stylesheet">
+  <link href="{{ asset('template_manual_book/assets/css/main.css?v=1.0') }}" rel="stylesheet">
 
   <style>
     .pagination-container {
